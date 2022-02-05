4167e9fec211e4b9940dafbd3492448119a480e4
hazysu <hazycora@protonmail.com>
2022-02-05 10:16:25 -0600

    use port env var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ftp = require('basic-ftp')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000
 
 app.get('/*', async (req, res) => {
     let path = decodeURI(req.path)
