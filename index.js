2b9d3dd7c465de5f0f959b8f08e992a8dfd19ee3
hazysu <hazycora@protonmail.com>
2022-02-05 10:13:13 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const ftp = require('basic-ftp')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/*', async (req, res) => {
+    let path = decodeURI(req.path)
+    if (path.endsWith('/')) var isDir = true
+    else var isDir = false
+    if (!req.query.host) return res.json({msg: 'no host specified'})
+    let client = new ftp.Client()
+    try {
+        await client.access({
+            host: req.query.host,
+            user: req.query.user,
+            password: req.query.pass,
+            secure: req.query.secure||false
+        })
+        if (isDir) {
+            await client.cd(path)
+            res.json({
+                currentDir: await client.pwd(),
+                list: await client.list()
+            })
+        } else {
+            await client.downloadTo(res, path)
+        }
+        client.close()
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({err: true, info: error, stack: error.stack})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
